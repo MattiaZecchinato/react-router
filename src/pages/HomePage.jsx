c889590f602057a0169bf683eb073bfd2f75b46e
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-16 15:32:26 +0200

    feat: add element to HomePage page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,13 @@
 export default function HomePage() {
 
-    return <h1>Home Page</h1>
+    return <div className="container home-page-container">
+        <h1>Home Page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Odio ea fugiat quos 
+            animi natus ipsa quod perferendis mollitia 
+            nesciunt dolores, itaque repellat rem unde 
+            est temporibus tempora possimus, cumque minus.
+        </p>
+    </div>
 }
