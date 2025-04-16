45fe28b80b4393365c9d6637fbe0210d34747ca6
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-16 15:32:49 +0200

    feat: add element to AboutUs page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,4 +1,13 @@
 export default function AboutUs() {
 
-    return <h1>About Us</h1>
+    return <div className="container about-us-container">
+        <h1>About Us</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Odio ea fugiat quos 
+            animi natus ipsa quod perferendis mollitia 
+            nesciunt dolores, itaque repellat rem unde 
+            est temporibus tempora possimus, cumque minus.
+        </p>
+    </div>
 }
