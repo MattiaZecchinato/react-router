3b43d5f7835833e7e67ef66ee22f844617a6f536
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-17 15:08:52 +0200

    feat: add link for single post

--- src/components/PostListCard.jsx
+++ src/components/PostListCard.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 // css
 import style from './PostListCard.module.css';
 
-export default function PostCard({ title, content }) {
+export default function PostCard({ id, title, content }) {
 
-    return <div className={style.cardBox}>
-        <h2>{title}</h2>
-        <p>{content}.</p>
-    </div>
+    const linkPost = `/PostList/${id}`;
+
+    return <Link to={linkPost} className={style.cardBox}>
+                 <h2>{title}</h2>
+                 <p>{content}.</p>
+    </Link>
 }
